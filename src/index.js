393fab397276edede932c7f8c4d75ebe719c8a2c
realabdullah <abdulodesanmi@gmail.com>
2023-11-04 01:59:19 +0100

    Add cors package and middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import user from './routes/user.js';
+import cors from 'cors';
 import 'dotenv/config';
+// eslint-disable-next-line no-unused-vars
+import mongoose from './db.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/users', user);
 
