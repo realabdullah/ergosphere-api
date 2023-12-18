512b1e8fd93b81a36785874acee25f81d62ced56
realabdullah <abdulodesanmi@gmail.com>
2023-12-18 12:57:05 +0100

    feat: migrate chats collection to comments collection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 import express from 'express';
 import bodyParser from 'body-parser';
 import auth from './routes/authRoutes.js';
@@ -11,6 +12,22 @@ import 'dotenv/config';
 // eslint-disable-next-line no-unused-vars
 import mongoose from './db.js';
 
+const CommentSchema = new mongoose.Schema({
+    message: {
+        type: String,
+        require: true,
+    },
+    task: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    createdAt: Date,
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -23,5 +40,21 @@ app.use('/workspaces', workspace);
 app.use('/tasks', task);
 app.use('/teams', team);
 app.use('/invite', invite);
+
+async function migrateCollections(sourceCollectionName, destinationCollectionName) {
+    try {
+        const SourceModel = mongoose.model(sourceCollectionName, CommentSchema);
+        const DestinationModel = mongoose.model(destinationCollectionName, CommentSchema);
+
+        const documents = await SourceModel.find().lean().exec();
+        await DestinationModel.insertMany(documents);
+
+        console.log(`Migration complete: ${documents.length} documents migrated.`);
+    } catch (error) {
+        console.error('Migration error:', error);
+    }
+}
+
+await migrateCollections('chats', 'comments');
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
